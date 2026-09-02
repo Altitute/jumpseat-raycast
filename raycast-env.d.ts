1a03193c1e8ba8c1d393d5b97bb34d0cee08f364
Patryk Radziszewski <patryk@chef.ski>
2026-09-02 21:27:06 +0100

    feat: add friends' upcoming flights view (#5)

--- raycast-env.d.ts
+++ raycast-env.d.ts
@@ -20,6 +20,8 @@ declare type Preferences = ExtensionPreferences
 declare namespace Preferences {
   /** Preferences accessible in the `upcoming-flights` command */
   export type UpcomingFlights = ExtensionPreferences & {}
+  /** Preferences accessible in the `friends-upcoming-flights` command */
+  export type FriendsUpcomingFlights = ExtensionPreferences & {}
   /** Preferences accessible in the `next-flight-in-menu-bar` command */
   export type NextFlightInMenuBar = ExtensionPreferences & {}
 }
@@ -27,6 +29,8 @@ declare namespace Preferences {
 declare namespace Arguments {
   /** Arguments passed to the `upcoming-flights` command */
   export type UpcomingFlights = {}
+  /** Arguments passed to the `friends-upcoming-flights` command */
+  export type FriendsUpcomingFlights = {}
   /** Arguments passed to the `next-flight-in-menu-bar` command */
   export type NextFlightInMenuBar = {}
 }
